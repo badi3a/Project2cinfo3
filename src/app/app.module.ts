35c4ae407fb02fd80c5ad4cf4544a777649e17a5
badi3a <badiaabouhdid@gmail.com>
2022-02-21 20:06:59 +0100

    commit S2.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule
